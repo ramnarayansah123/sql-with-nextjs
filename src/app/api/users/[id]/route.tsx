ae12c7a7c150eb001eca7f3a7e75f3dde6e1a1c6
Ramnarayan <ramnarayansah180@gmail.com>
2025-01-18 08:30:34 +0545

    this is working and modifed

--- src/app/api/users/[id]/route.tsx
+++ src/app/api/users/[id]/route.tsx
@@ -1,6 +1,34 @@
 import { NextRequest, NextResponse } from "next/server";
-import { Prisma } from '@prisma/client'
+import { prisma } from "../../../../../prisma/client";
 
+
+// This is for GET METHOD
 export async function GET(request:NextRequest){
   const users =await prisma.user.findMany()
     return NextResponse.json(users)}
+
+    // this for POST METHOD 
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+
+    // Create a new user in the database
+    const user = await prisma.user.create({
+      data: {
+        name: body.name,
+        email: body.email,
+      },
+    });
+
+    // Return the newly created user with a status of 201
+    return NextResponse.json(user, { status: 201 });
+  } catch (error) {
+    console.error("Error creating user:", error);
+
+    // Return an error response with a status of 500
+    return NextResponse.json(
+      { error: "An error occurred while creating the user." },
+      { status: 500 }
+    );
+  }
+}
